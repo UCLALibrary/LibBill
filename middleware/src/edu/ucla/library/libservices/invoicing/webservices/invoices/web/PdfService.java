5b2a19f7b8f283600bc8e2ca6f3c05069e7057c5
David Rickard <drickard1967@library.ucla.edu>
2019-11-25 10:49:45 -0800

    VBT-1454 adding mailing
    adding mailing to simple pdf service

--- middleware/src/edu/ucla/library/libservices/invoicing/webservices/invoices/web/PdfService.java
+++ middleware/src/edu/ucla/library/libservices/invoicing/webservices/invoices/web/PdfService.java
@@ -5,9 +5,20 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import edu.ucla.library.libservices.invoicing.utiltiy.mail.FileMailer;
+import edu.ucla.library.libservices.invoicing.utiltiy.testing.ContentTests;
 import edu.ucla.library.libservices.invoicing.webservices.invoices.generator.PdfGenerator;
 
 import java.io.ByteArrayOutputStream;
+
+import java.io.File;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
 
 import javax.servlet.ServletConfig;
 
@@ -59,4 +70,60 @@ public class PdfService
     }
   }
 
+  @GET
+  @Path( "/mail_invoice/{in}" )
+  public Response mailInvoicePdf( @PathParam( "in" )
+    String invoiceNo )
+  {
+    File file;
+    FileOutputStream output;
+    Document document;
+    PdfGenerator generator;
+    StringBuffer fileName;
+    try
+    {
+      generator = new PdfGenerator();
+      generator.setInvoiceNumber( invoiceNo ); 
+      generator.setDbName( config.getServletContext().getInitParameter( "datasource.invoice" ) );
+
+      fileName =
+          new StringBuffer( config.getServletContext().getInitParameter( "mail.filebase" ) ).append( invoiceNo ).append( ".pdf" );
+      file = new File( fileName.toString() );
+      document = new Document( PageSize.LETTER );
+      output = new FileOutputStream( file );
+
+      PdfWriter.getInstance( document, output );
+      generator.populatePdf( document );
+
+      if ( !ContentTests.isEmpty( generator.getTheInvoice().getPatron().getEmail() ) )
+      {
+        FileMailer.mailFile( invoiceNo.concat( ".pdf" ),
+                             generator.getTheInvoice().getPatron().getEmail(),
+                             config.getServletContext().getInitParameter( "mail.propsfile" ) );
+        file.delete();
+        return Response.ok().build();
+      }
+      else
+      {
+        file.delete();
+        return Response.status( Response.Status.NOT_FOUND ).entity( "Patron lacks an email address" ).build();
+      }
+    }
+    catch ( DocumentException de )
+    {
+      return Response.serverError().entity( "Invoice creation failed: ".concat( de.getMessage() ) ).build();
+    }
+    catch ( FileNotFoundException fnfe )
+    {
+      return Response.serverError().entity( "Invoice creation failed: ".concat( fnfe.getMessage() ) ).build();
+    }
+    catch ( IOException ioe )
+    {
+      return Response.serverError().entity( "Invoice mailing failed: ".concat( ioe.getMessage() ) ).build();
+    }
+    catch ( MessagingException me )
+    {
+      return Response.serverError().entity( "Invoice mailing failed: ".concat( me.getMessage() ) ).build();
+    }
+  }
 }
