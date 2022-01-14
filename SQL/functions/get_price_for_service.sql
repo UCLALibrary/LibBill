d34b42c16356fb21188dc62e4bf86921d6cfaac0
Andy Kohler <akohler@library.ucla.edu>
2022-01-13 15:58:41 -0900

    SYS-703: Handle UC membership correctly in database (#7)
    * SYS-703: Handle UC membership correctly after move to Alma
    * SYS-703: Move optional unit price parameter to final position in procedure declarations
    * SYS-703: Add schema change script for 2.1.1 to 2.1.2

--- SQL/functions/get_price_for_service.sql
+++ SQL/functions/get_price_for_service.sql
@@ -1,19 +1,15 @@
 create or replace function get_price_for_service (
   p_invoice_number in line_item.invoice_number%type
 , p_location_service_id in line_item.location_service_id%type
+, p_is_uc_member in char
 ) return location_service.uc_price%type as
   v_patron_id invoice.patron_id%type;
   v_price location_service.uc_price%type;
 begin
-  -- Get the patron for this invoice
-  select patron_id into v_patron_id
-    from invoice
-    where invoice_number = p_invoice_number;
-
   -- Get the UC or non-UC price for this service, based on patron id
   select 
-    case is_patron_uc(v_patron_id)
-      when 1 then uc_price 
+    case p_is_uc_member
+      when 'Y' then uc_price
       else non_uc_price 
     end into v_price
   from location_service
